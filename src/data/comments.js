846d6649187ee031b25c3c6b3e51dfe44a50f822
LesRedfield <L41492@gmail.com>
2017-06-01 14:39:56 -0400

    Add React front end

---
+++ src/data/comments.js
@@ -0,0 +1,30 @@
+const comments = {
+  "BAhvZrRwcfu":[
+    {
+      "text":"You're doing it wrong.",
+      "user": "JBinder919"
+    }
+  ],
+  "BAcyDyQwcXX":[
+    {
+      "text":"Did that even go in?",
+      "user": "Otto-P"
+    },
+    {
+      "text":"Bet your ass it did!",
+      "user": "kingjames23"
+    }
+  ],
+  "BAPIPRjQce9":[
+    {
+      "text":"He's a warrior killer!",
+      "user": "brdmn"
+    },
+    {
+      "text":"Sucks to be those fans",
+      "user": "knicks_guy_73"
+    }
+  ]
+};
+
+export default comments;
---
+++ src/data/posts.js
@@ -0,0 +1,47 @@
+const posts = [
+   {
+      "code":"BAcyDyQwcXX",
+      "caption":"Play it like a song!",
+      "likes":56,
+      "id":"1161022966406956503",
+      "display_src":"https://lh6.googleusercontent.com/-24-N4DZIl-o/AAAAAAAAAAI/AAAAAAAAHVk/0MHWykKAbbQ/s0-c-k-no-ns/photo.jpg"
+   },
+   {
+      "code":"BAcJeJrQca9",
+      "caption":"Pound it!",
+      "likes":59,
+      "id":"1160844458347054781",
+      "display_src":"https://media.tenor.co/images/8cbb6538ffae107647ed0c5ba52961c1/raw"
+   },
+   {
+      "code":"BAF_KY4wcRY",
+      "caption":"Hi, Gibbles.",
+      "likes":79,
+      "id":"1154606670337393752",
+      "display_src":"https://usatftw.files.wordpress.com/2017/02/usatsi_9863193_168380427_lowres.jpg"
+   },
+   {
+      "code":"BAPIPRjQce9",
+      "caption":"Bad Offense",
+      "likes":47,
+      "id":"1157179863266871229",
+      "display_src":"https://usatthebiglead.files.wordpress.com/2014/02/lebron-james-game-winning-3-against-golden-state.gif"
+   },
+   {
+      "code":"-hZh6IQcfN",
+      "caption":"What goes up...",
+      "likes":66,
+      "id":"1126293663140399053",
+      "display_src":"http://fansided.com/files/2016/01/lebron-james-nba-cleveland-cavaliers-philadelphia-76ers-3.jpg"
+   },
+   {
+      "code":"-B3eiIwcYV",
+      "caption":"...must come down.",
+      "likes":33,
+      "id":"1117418173361145365",
+      "display_src":"https://usatthebiglead.files.wordpress.com/2014/02/lebron-james-celebrates-game-winning-3-against-golden-state.gif?w=640&h=369"
+   }
+];
+
+
+export default posts;
